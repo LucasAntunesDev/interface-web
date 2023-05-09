c0fb23832ee454467a95241bee8c0a6a1d01c6ef
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-05-09 17:26:46 -0300

    jogo da velha atualizado

--- 5 - Maio/script.js
+++ 5 - Maio/script.js
@@ -1,38 +1,42 @@
-criarTabuleiro = (max = 16) => {
+const criarTabuleiro = (max = 16) => {
   if (max % 2 == 1) {
-    console.log("O número tem que ser par");
+    console.log("O número tem que ser par!");
     return;
   }
-  //Cria um array do tabuleiro
   const tab = [];
 
   let k = 0;
   while (k < max) {
     const n = Math.ceil(Math.random() * (max / 2));
     if (tab.filter((x) => x === n).length >= 2) continue;
+
     tab.push(n);
     k++;
   }
+
   return tab;
 };
 
-const jogadores = ["Jogador 1", "Jogador 2", "Jogador 3"];
-const spans = []
-//Qual é o jogador atual
+const jogadores = ["Jogador 1", "Jogador 2"];
+const spans = [];
 let jogador = 0;
-//Histórico de jogadas
 let jogadas = [];
+let placar = [];
+let trancarJogo = false;
 
-const divJ = document.getElementById('jogadores')
-jogadores.forEach(j => {
-    const span = document.getElementById('span')
-    span.innerHTML = j
-    divJ.appendChild(span)
-    span.appendChild(div)
-})
+const divJ = document.getElementById("jogadores");
+jogadores.forEach((j) => {
+  const span = document.createElement("span");
+  span.innerHTML = j;
+  const i = document.createElement("I");
+  i.innerHTML = "0";
+  placar.push(i);
+  span.appendChild(i);
+  divJ.appendChild(span);
+  spans.push(span);
+});
 
-const nome = document.getElementById("nome");
-nome.innerHTML = jogadores[jogador];
+spans[jogador].classList.add("jogadorAtivo");
 
 const tab = criarTabuleiro();
 const div = document.getElementById("tabuleiro");
@@ -42,22 +46,47 @@ tab.forEach((n) => {
   btn.setAttribute("type", "button");
   btn.addEventListener("click", () => {
     btn.innerHTML = n;
+
+    if (jogadas.some((j) => j === btn)) return;
+    if( trancarJogo == true) return;
     jogadas.push(btn);
 
     if (jogadas.length === 2) {
+      trancarJogo = true;
+
+
       if (jogadas[0].innerHTML === jogadas[1].innerHTML) {
-          jogadas = [];
-          alert("Você acertou!");
+        placar[jogador].innerHTML = parseInt(placar[jogador].innerHTML) + 1;
+        jogadas[0].disabled = true;
+        jogadas[1].disabled = true;
+        trancarJogo = false;
+
+        const p1 = parseInt(placar[0].innerHTML);
+        const p2 = parseInt(placar[1].innerHTML);
+
+        if (p1 + p2 === 8) {
+          if (p1 > p2) {
+            alert("Jogador 1 venceu");
+          } else if (p1 < p2) {
+            alert("Jogador 2 venceu");
+          } else {
+            alert("O jogo terminou empatado");
+          }
+        }
+
+        console.log("Você acertou!");
+        jogadas = [];
       } else {
-        alert("Você errou!");
+        console.log("Você errou!");
         jogador = (jogador + 1) % jogadores.length;
-        nome.innerHTML = jogadores[jogador];
+        spans.forEach((s) => s.classList.remove("jogadorAtivo"));
+        spans[jogador].classList.add("jogadorAtivo");
         setTimeout(() => {
           jogadas.forEach((b) => (b.innerHTML = ""));
           jogadas = [];
+          trancarJogo = false
         }, 1000);
       }
-
     }
   });
 
