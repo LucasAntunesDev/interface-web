be5e048d925a47192526ee7af0a7a2735e9fd363
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-08-22 12:32:15 -0300

    style: update header

--- semestre_2/8 - Agosto/trabalho_2/header.php
+++ semestre_2/8 - Agosto/trabalho_2/header.php
@@ -1,6 +1,6 @@
 <header class="flex justify-between items-center gap-x-8 px-8 h-16
     text-zinc-50 grow z-20 treansition duration-300 ease-in-out
-    bg-red-800 border-b-red-600 border-b-[3px] shadow-md sticky top-0">
+    bg-red-800 shadow-md sticky top-0">
 
     <a href="index.php">
         <img src="assets/logo.png" alt="ACL Móveis" class="h-16 hover:scale-110 brightness-[100]">
@@ -8,7 +8,6 @@
 
     <div class="flex justify-between items-center gap-x-8 h-10">
         <span class="align-baseline">
-            <!-- <a href="sobre.php">Sobre</a> -->
             <a href="sobre.php" class="hover-effect">Sobre</a>
         </span>
 
@@ -17,12 +16,7 @@
         </span>
 
         <span class="align-baseline">
-            <!-- <a href="produtos.php">Produtos</a> -->
             <a href="produtos.php" class="hover-effect">Produtos</a>
         </span>
 
 </header>
-<!-- <header class="sticky top-0 bg-blue-500 shadow-lg p-4">
-    <h1 class="text-white text-xl font-bold">Meu Header Flutuante</h1>
-    <p class="text-white text-sm">Este é um exemplo de header flutuante com html e tailwindcss.</p>
-  </header> -->
