2900e96e37794260366b9a41debc1ea24c94f23b
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-08-22 14:52:32 -0300

    update

--- semestre_2/8 - Agosto/trabalho_2/footer.php
+++ semestre_2/8 - Agosto/trabalho_2/footer.php
@@ -1,11 +1,27 @@
-<footer class="flex justify-between items-center gap-x-8 px-8 h-16
+<footer class="flex justify-between items-center gap-x-8 px-8 min-h-16
     text-zinc-50 grow z-20 treansition duration-300 ease-in-out
-    bg-red-800 shadow-md mt-8">
+    bg-red-800 shadow-md mt-8 pb-2">
 
-    <span>ACL Móveis Sob Medida LTDA.</span>
-    
-    <span>Rua Andrea Vitorre Mazzolini, 187 Distrito Industria de Tamandaré, CEP 95720-000 - Garibaldi - RS  </span>
+    <div class="flex flex-col justify-center">
+        <img src="assets/logo.png" alt="Logo da ACL Móveis Sob Medida" class="brightness-[100] h-20">
+        <span>ACL Móveis Sob Medida LTDA.</span>
+    </div>
 
-   <span>(54) 3464-7111 ou (54) 99610-8684</span>
+
+    <div class="flex flex-col justify-center">
+        <h4 class="font-bold text-2xl">Contato</h4>
+        <span>(54) 3464-7111 ou (54) 99610-8684</span>
+        <span class="w-6/12">Rua Andrea Vitorre Mazzolini, 187 Distrito Industria de Tamandaré, CEP 95720-000 - Garibaldi - RS </span>
+
+        <div class="flex gap-x-2">
+            <a href="#">
+                <span class="fa-brands fa-instagram text-2xl hover:text-zinc-50/50 transition duration-300 ease-in-out"></span>
+            </a>
+
+            <a href="#">
+                <span class="fa-brands fa-facebook text-2xl hover:text-zinc-50/50 transition duration-300 ease-in-out"></span>
+            </a>
+        </div>
+    </div>
 
 </footer>
