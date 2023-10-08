583cb384de6a495ad35ee59ff4987be14b35bb6d
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-10-08 17:45:13 -0300

    update

--- semestre_2/9 - Setembro/trabalho_responsividade/script.js
+++ semestre_2/9 - Setembro/trabalho_responsividade/script.js
@@ -1,8 +1,8 @@
 const hamburger = document.querySelector('#hamburger-btn')
 hamburger.addEventListener('click', () => {
-    // alert('oi')
+
     const menu = document.querySelector('#mobile-menu')
-    // menu.classList.contains('visible') ? menu.classList.remove("visible") :menu.classList.add("visible")
+    
     if (menu.classList.contains('visible')) {
         menu.classList.remove("visible")
         menu.style.display = 'none'
